fb540f6e28c83b55e886e3ac215331e9c38ab90a
hegderguru <hegde.r.guru@gmail.com>
2026-07-18 22:28:13 +0530

    update

--- src/main/java/com/karur/realty_management_application/repository/SiteRealtyAssetEntityRepository.java
+++ src/main/java/com/karur/realty_management_application/repository/SiteRealtyAssetEntityRepository.java
@@ -1,9 +1,17 @@
 package com.karur.realty_management_application.repository;
 
 import com.karur.realty_management_application.entity.asset.realty.SiteRealtyAssetEntity;
+import com.karur.realty_management_application.entity.party.organisation.OrganisationEntity;
+import com.karur.realty_management_application.entity.party.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SiteRealtyAssetEntityRepository extends JpaRepository<SiteRealtyAssetEntity,Long> {
+    Optional<UserEntity> findUserEntityByBelongsToId(Long id);
+    Optional<OrganisationEntity> findOrganisationEntityByBelongsToId(Long id);
+    Optional<OrganisationEntity> findOrganisationEntityByParentSiteRealtyAssetEntityId(Long id);
+    Optional<UserEntity> findUserEntityByParentSiteRealtyAssetEntityBelongsToId(Long id);
 }
---
+++ src/main/java/com/karur/realty_management_application/repository/UserEntityRepository.java
@@ -0,0 +1,16 @@
+package com.karur.realty_management_application.repository;
+
+import com.karur.realty_management_application.entity.party.user.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserEntityRepository extends JpaRepository<UserEntity,Long> {
+    Optional<UserEntity> findByUsername(String username);
+    Optional<List<UserEntity>> findByOrganisationEntityId(Long organisationId);
+    Optional<List<UserEntity>> findByOrganisationEntityNumber(String number);
+    Optional<List<UserEntity>> findByOrganisationEntityName(String name);
+}
