0c302da1b4e66a9aae75b75033a10aa49ea92ce1
hegderguru <hegde.r.guru@gmail.com>
2026-07-19 01:19:08 +0530

    update

---
+++ src/main/java/com/karur/realty_management_application/model/response/AssetResponse.java
@@ -0,0 +1,33 @@
+package com.karur.realty_management_application.model.response;
+
+import com.karur.realty_management_application.model.read.UserDetail;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+public class AssetResponse {
+    private HttpStatus code;
+    private String message;
+    private UserDetail userDetail;
+
+    public AssetResponse(HttpStatus code,String message,UserDetail userDetail){
+        this.code=code;
+        this.message=message;
+        this.userDetail=userDetail;
+    }
+
+    public static class SuccessResponse extends AssetResponse{
+        public SuccessResponse(HttpStatus code,String message,UserDetail userDetail){
+            super(code,message,userDetail);
+        }
+    }
+
+    public static class FailureResponse extends AssetResponse{
+        public FailureResponse(HttpStatus code,String message,UserDetail userDetail){
+            super(code,message,userDetail);
+        }
+    }
+}
